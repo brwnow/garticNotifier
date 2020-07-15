972779f5f3960f46f5de9bf5c4179079d19b50f1
Bruno Garcia <brwnow@gmail.com>
2020-07-15 02:28:14 -0300

    Adding temporary main.py useful for testing classes as they're developed

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+from bs4 import BeautifulSoup
+from garticrequester import GarticRequester
+
+if len(sys.argv) > 1:
+    user = sys.argv[1]
+else:
+    user = ""
+
+friendsReq = ""
+content = ""
+
+if len(user) == 0:
+    print("No user defined")
+    exit()
+else:
+    garticRequester = GarticRequester()
+    content = garticRequester.getFriendsPage(user, "1")
+
+soup = BeautifulSoup(content, 'lxml')
+
+for player in soup.find_all("div", class_="titulo"):
+    print(player.find("a").contents[0].replace("\n", "").lower())
