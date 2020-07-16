40d74edaa7f878832abd46f0795914050a5d837e
Bruno Garcia <brwnow@gmail.com>
2020-07-16 03:59:16 -0300

    Updating default gartic page GET request header to a more complete header format

--- garticrequester.py
+++ garticrequester.py
@@ -3,11 +3,16 @@ import urllib.request
 class GarticRequester:
     GARTIC_BASE_URL = "https://www.gartic.com.br"
 
-    HTTP_REQUEST_HEADER = {  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-                'Accept-Encoding': 'none',
-                'Accept-Language': 'en-US,en;q=0.8' }
+    HTTP_REQUEST_HEADER = { 'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+                            'method': 'GET',
+                            'scheme': 'https',
+                            'sec-fetch-dest': 'document',
+                            'sec-fetch-mode': 'navigate',
+                            'sec-fetch-site': 'none',
+                            'upgrade-insecure-requests': '1',
+                            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                            'accept-language': 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+                            'connection': 'close' }
 
     GARTIC_FRIENDS_ENDPOINT = "/amigos"
     GARTIC_FRIENDS_PAG_ATT = "pag"
