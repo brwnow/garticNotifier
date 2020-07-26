65a2494af176edb67ac27ae09d91e8adac1eccfa
Bruno Garcia <brwnow@gmail.com>
2020-07-26 17:33:59 -0300

    Refactoring CoreApplication class
    Splitting its logic in more methods. Also making some variables as attributes. Moved some logic to inside of Algorithm class to make CoreApplication cleaner

--- src/core/coreapplication.py
+++ src/core/coreapplication.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import keyboard
+import argparse
 from playsound import playsound
 
 from utils import log
@@ -15,67 +16,61 @@ def onQuitKeyPressed():
     shouldStop = True
 
 class CoreApplication:
+    __GARTIC_REQUESTS_INTERVAL = 60
+    __user = None
     __algorithm = Algorithm()
+    __garticRequester = GarticRequester()
 
     def __init__(self):
-        pass
+        keyboard.add_hotkey('ctrl+shift+q', onQuitKeyPressed)
+
+    def enableFriendsActivityNotifications(self, userName):
+        self.__user = userName
 
     def runApplication(self):
-        GARTIC_REQUESTS_INTERVAL = 60 # in seconds
-        user = ""
-
         # Checking if a username was passed ass the first parameter
         if len(sys.argv) > 1:
-            user = sys.argv[1]
+            self.__user = sys.argv[1]
         else:
             print("Username is missing. Pass the username as the first parameter of the program call")
             exit()
 
-        keyboard.add_hotkey('ctrl+shift+q', onQuitKeyPressed)
-
-        garticRequester = GarticRequester()
-
-        algorithm = Algorithm()
-        timestamp = time.time() - GARTIC_REQUESTS_INTERVAL
-        oldList = []
+        timestamp = time.time() - self.__GARTIC_REQUESTS_INTERVAL
         while not shouldStop:
-            if time.time() - timestamp >= GARTIC_REQUESTS_INTERVAL:
+            if time.time() - timestamp >= self.__GARTIC_REQUESTS_INTERVAL:
                 timestamp = time.time()
 
-                log.log(user + '\'s friends page 1 request attempt')
-                friendsPageRaw = garticRequester.getFriendsPageHtml(user, "1")
-
-                friendsPage = None
-
-                if friendsPageRaw != None:
-                    log.log('Parsing ' + user + ' first page of friends')
-                    friendsPage = GarticFriendPage(friendsPageRaw)
-                else:
-                    log.log('Requesting ' + user + ' friends page failed')
-                    continue
-
-                if len(oldList) > 0:
-                    log.log('Detecting friends of ' + user + ' that have logged since last check')
-                    recentLoggedUsers = algorithm.extractRecentLoggedUsers(friendsPage.friendsList, oldList)
-
-                    oldList = friendsPage.friendsList
-
-                    if len(recentLoggedUsers) > 0:
-                        log.log('Friends login activity detected!')
-
-                        playsound("../media/sound/notification.wav")
-
-                        loggedUsersMsg = time.strftime('[%X]')
-
-                        for friend in recentLoggedUsers:
-                            loggedUsersMsg = loggedUsersMsg + ' | ' + friend
-
-                        print(loggedUsersMsg)
-                    else:
-                        log.log('No friends activity')
-
-                else:
-                    log.log('Storing current friends list order for further login detection')
-                    oldList = friendsPage.friendsList
+                self.__checkFriendsActivity()
 
             time.sleep(.05)
+
+    def __checkFriendsActivity(self):
+        log.log(self.__user + '\'s friends page 1 request attempt')
+        friendsPageRaw = self.__garticRequester.getFriendsPageHtml(self.__user, "1")
+
+        friendsPage = None
+
+        if friendsPageRaw != None:
+            log.log('Parsing ' + self.__user + ' first page of friends')
+            friendsPage = GarticFriendPage(friendsPageRaw)
+        else:
+            log.log('Requesting ' + self.__user + ' friends page failed')
+            return
+
+
+        log.log('Detecting friends of ' + self.__user + ' that have logged since last check')
+        recentLoggedUsers = self.__algorithm.extractRecentLoggedUsers(friendsPage.friendsList)
+
+        if len(recentLoggedUsers) > 0:
+            log.log('Friends login activity detected!')
+
+            playsound("../media/sound/notification.wav")
+
+            loggedUsersMsg = time.strftime('[%H:%M]')
+
+            for friend in recentLoggedUsers:
+                loggedUsersMsg = loggedUsersMsg + ' | ' + friend
+
+            print(loggedUsersMsg)
+        else:
+            log.log('No friends activity')
