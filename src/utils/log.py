64f95e0ef87b7767bf8af453963145ffe9e9e25e
Bruno Garcia <brwnow@gmail.com>
2020-07-26 22:43:35 -0300

    Refactoring log functions. Making it possible to set log level

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,7 +1,11 @@
-logEnabled = False
+logLevel = 0
 
 def log(msg):
-    if logEnabled == True:
+    if logLevel > 0:
         print(msg)
     else:
         pass
+
+def setLogLevel(value):
+    global logLevel
+    logLevel = value
